c631780ffb6f3c02bfcef1f71a817e79959eb23a
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2025-01-29 13:00:28 +0530

    Add files via upload

---
+++ If_Else_Statement.py
@@ -0,0 +1,9 @@
+#If Else statement
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("you can ride the rollercoaster!")
+else:
+    print("sorry you have to grow taller for the ride!")
